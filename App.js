b1d02ad4c5166ec4f1df14f3e7fe7acde3f83b92
Exile <JasonParks.g4g@gmail.com>
2023-05-16 11:40:30 -0500

    updated bottom nav bar

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import Display from './components/displayTask';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Details } from './screens/selectTaskScreen';
 import { HomeScreen } from './screens/homeScreen';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer style={styles.container}>
-      <Tab.Navigator initialRouteName='Home' swipeEnabled style={{backgroundColor: 'blue'}}>
-        <Tab.Screen name='Create Task' component={Display} />
-        <Tab.Screen name='Home' component={HomeScreen} />
-        <Tab.Screen name="Details" component={Details} />
+      <Tab.Navigator 
+        initialRouteName='Home' 
+        screenOptions={{ 
+          tabBarActiveTintColor: 'orange',
+        }}
+      >
+        <Tab.Screen 
+          name='Create Task' 
+          component={Display} 
+          options={{  tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons 
+                name="plus" 
+                color={color} 
+                size={size} 
+              />
+            ), 
+          }}
+        />
+        <Tab.Screen 
+          name='Home' 
+          component={HomeScreen} 
+          options={{tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons 
+                name="home" 
+                color={color} 
+                size={size} 
+              />
+            ),
+          }}
+        />
+        <Tab.Screen 
+          name="Details" 
+          component={Details} 
+          options={{ tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons 
+                name="menu" 
+                color={color} 
+                size={size} 
+              />
+            ), 
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
@@ -27,8 +67,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  tabBar: {
-    activeTintColor: '#FFC300',
-
+  tabBarOptions: {
+    activeTintColor: '#e91e63',
+    labelStyle: {
+      fontSize: 15,
+    },
+    style: {
+      backgroundColor: 'red',
+    },
   }
 });
