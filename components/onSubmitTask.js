78b4a55e0193143a052e423b0c87ddffd077683b
Exile <JasonParks.g4g@gmail.com>
2023-05-16 21:48:11 -0500

    fixing task submit

--- components/onSubmitTask.js
+++ components/onSubmitTask.js
@@ -1,17 +1,11 @@
 import { Input, Card, ListItem, Button, Icon } from 'react-native-elements';
 import { StyleSheet, Text, View } from 'react-native';
 import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { submitTask } from './submitTask';
 
 const onSubmitTask = () => {
-    const dispatch = useDispatch();
+    const [task, setTask] = useState('');
 
-    const newTask = {
-        task
-    };
-    dispatch(submitTask(newTask))
-    console.log('Button pushed');
+    
 };
 
 export default onSubmitTask;
